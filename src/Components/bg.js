d8c49e65d779c600f11149e547144a176b59ad33
Eya Yalung <allyougottadoismagic@gmail.com>
2025-11-25 02:54:33 +0800

    polished part 4

--- src/Components/bg.js
+++ src/Components/bg.js
@@ -1,16 +1,10 @@
 import '../Styles/bg.css';
-import wmpIcon from "../Assets/imgs/wmp.png";
-import homeIcon from "../Assets/imgs/vista_info.ico";
 
 function Bg(){
-    const icons = [
-    { id: 1, title: "Home", icon: homeIcon },
-    { id: 2, title: "Media Player", icon: wmpIcon },];
-    
-    return <div>
+    return(
         <div className="bg-img">
         </div>
-    </div>
+    );
 }
 
 export default Bg;
