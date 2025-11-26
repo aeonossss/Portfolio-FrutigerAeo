e5681579d7d0b4c9aa5e1162f65d1054a95d423b
Eya Yalung <allyougottadoismagic@gmail.com>
2025-11-26 20:26:00 +0800

    added mobile banner

--- src/Components/bg.js
+++ src/Components/bg.js
@@ -1,8 +1,21 @@
 import '../Styles/bg.css';
+import { useState } from 'react'
 
 function Bg(){
+    const [showBanner, setShowBanner] = useState(true);
+
     return(
         <div className="bg-img">
+            {showBanner && (
+                <div className='responsive-banner'>
+                The site provides a better experience when viewed on a desktop.
+                <button 
+                    className='ok-button'
+                    onClick={() =>setShowBanner(false)}>
+                        OK
+                </button>
+            </div>
+            )}
         </div>
     );
 }
